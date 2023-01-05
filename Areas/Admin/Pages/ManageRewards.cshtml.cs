f74eb5daefab58e93c8778802ed5113f847dd342
xegrox <xegrox@protonmail.com>
2023-01-05 17:16:14 +0800

    feat(admin): impl view all rewards tab

--- Areas/Admin/Pages/ManageRewards.cshtml.cs
+++ Areas/Admin/Pages/ManageRewards.cshtml.cs
@@ -7,21 +7,29 @@ namespace ReACT.Areas.Admin.Pages;
 
 public class ManageRewards : PageModel
 {
-    public IActionResult OnGet(int? categoryId)
+    public IActionResult OnGet(int? categoryId, bool all)
     {
-        categoryId ??= MockRewardsDb.Categories.FirstOrDefault()?.Id;
-        if (categoryId != null)
+        IEnumerable<Reward> rewards;
+        if (all)
         {
+            rewards = MockRewardsDb.Rewards.AsEnumerable();
+        }
+        else
+        {
+            categoryId ??= MockRewardsDb.Categories.FirstOrDefault()?.Id;
+            if (categoryId == null) return RedirectToPage("ManageRewards", new { all = true });
             var categoryExists = MockRewardsDb.Categories.Any(c => c.Id == categoryId);
             if (!categoryExists) return RedirectToPage();
-            var rewards = MockRewardsDb.Rewards.Where(r => r.CategoryId == categoryId);
-            foreach (var reward in rewards)
-            {
-                reward.Variants = MockRewardsDb.Variants.Where(v => v.RewardId == reward.Id).ToList();
-            }
-            ViewData["rewards"] = rewards;
+            rewards = MockRewardsDb.Rewards.Where(r => r.CategoryId == categoryId);
+            ViewData["activeCategoryId"] = categoryId;
         }
-        ViewData["activeCategoryId"] = categoryId;
+        
+        foreach (var reward in rewards)
+        {
+            reward.Variants = MockRewardsDb.Variants.Where(v => v.RewardId == reward.Id).ToList();
+        }
+
+        ViewData["rewards"] = rewards;
         ViewData["categories"] = MockRewardsDb.Categories;
         return Page();
     }
