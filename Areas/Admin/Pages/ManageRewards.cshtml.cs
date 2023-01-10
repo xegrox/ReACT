34defed66449005e7a986e4fa9cb3d6aaf1b39d6
xegrox <xegrox@protonmail.com>
2023-01-11 00:28:28 +0800

    feat(admin): rewards paging

--- Areas/Admin/Pages/ManageRewards.cshtml.cs
+++ Areas/Admin/Pages/ManageRewards.cshtml.cs
@@ -8,13 +8,15 @@ namespace ReACT.Areas.Admin.Pages;
 
 public class ManageRewards : PageModel
 {
-    public IActionResult OnGet(int? categoryId, bool all, string? search)
+    public IActionResult OnGet(int? categoryId, bool all, string? search, int pageIndex = 1)
     {
-        IEnumerable<Reward> rewards;
+        if (pageIndex < 1) return RedirectToPage(new {categoryId, all, search});
+        const int rewardsPerPage = 5;
+        List<Reward> rewards;
         if (all)
         {
-            rewards = search == null ? MockRewardsDb.Rewards.AsEnumerable()
-                : MockRewardsDb.Rewards.Where(r => r.Name.FuzzyMatch(search));
+            rewards = search == null ? MockRewardsDb.Rewards
+                : MockRewardsDb.Rewards.Where(r => r.Name.FuzzyMatch(search)).ToList();
         }
         else
         {
@@ -22,15 +24,21 @@ public class ManageRewards : PageModel
             if (categoryId == null) return RedirectToPage("ManageRewards", new { all = true });
             var categoryExists = MockRewardsDb.Categories.Any(c => c.Id == categoryId);
             if (!categoryExists) return RedirectToPage();
-            rewards = MockRewardsDb.Rewards.Where(r => r.CategoryId == categoryId && (search == null || r.Name.FuzzyMatch(search)));
+            rewards = MockRewardsDb.Rewards.Where(r => r.CategoryId == categoryId && (search == null || r.Name.FuzzyMatch(search))).ToList();
             ViewData["activeCategoryId"] = categoryId;
         }
-        
+
+        var totalRewardsCount = rewards.Count;
+        if (rewardsPerPage * (pageIndex - 1) + 1 > totalRewardsCount) return RedirectToPage(new {categoryId, all, search});
+        rewards = rewards.Skip(rewardsPerPage * (pageIndex-1)).Take(rewardsPerPage).ToList();
         foreach (var reward in rewards)
         {
             reward.Variants = MockRewardsDb.Variants.Where(v => v.RewardId == reward.Id).ToList();
         }
-        
+
+        ViewData["rewardsPerPage"] = rewardsPerPage;
+        ViewData["pageIndex"] = pageIndex;
+        ViewData["pageCount"] = (totalRewardsCount - 1) / rewardsPerPage + 1;
         ViewData["search"] = search;
         ViewData["rewards"] = rewards;
         ViewData["categories"] = MockRewardsDb.Categories;
