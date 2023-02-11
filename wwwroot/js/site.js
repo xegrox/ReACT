15fced19987d44f34c3d9bf713faa83b2443e82b
xegrox <xegrox@protonmail.com>
2023-02-11 18:59:15 +0800

    feat(rewards): display variant stock overview in rewards list

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -19,6 +19,7 @@
         }, interval)
     })
     Alpine.directive('loading', (el, {expression}) => {
+        if (el.disabled) return
         const getLoading = Alpine.evaluateLater(el, expression)
         const spinner = document.createElement("div")
         spinner.innerHTML = '<svg class="animate-spin -ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">\n' +
